078b5b5fd024adeec8a1a516e26837fea3caefb3
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2019-07-20 22:43:57 +0200

    Updated

--- src/arduinoformat.cpp
+++ src/arduinoformat.cpp
@@ -1,27 +1,118 @@
+#ifdef WIN32
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#define min(x, y) x < y ? x : y
+#endif
+
 #include "arduinoformat.h"
 
 namespace fds {
 namespace format {
 
+typedef char* TextPointer;
+
+static TextPointer unit_ = nullptr;
+static TextPointer ids_ = nullptr;
+static uint8_t idslenght_ = 0;
+static uint8_t unitlenght_ = 0;
+static uint8_t fixedlenght_ = FORMAT_FIXED_LENGTH;
+static uint8_t width_ = FORMAT_DOUBLE_WIDTH;
+static uint8_t precision_ = FORMAT_DOUBLE_PRECISION;
+static uint8_t widthtouse, precisiontouse;
+static uint8_t size;
+static char idvaluesep_ = ' ';
+
 static char* formated[FORMAT_BUFFER_COUNT];
+static char* pointer = nullptr;
+
+static void assignbuffer(TextPointer& destination, const char* source, uint8_t& length, const uint8_t& sourcelength) {
+  length = sourcelength;
+  if(length > 0) {
+    if(destination != nullptr) {
+      free(destination);
+    }
+    destination = (char*)(malloc(length));
+    memcpy(destination, source, length);
+  } else {
+    if(destination != nullptr) {
+      free(destination);
+      destination = nullptr;
+    }
+  }
+}
+
 void setup() {
   for(uint8_t i = 0; i < FORMAT_BUFFER_COUNT; i++) {
     formated[i] = (char*)(malloc(FORMAT_BUFFER_SIZE));
   }
 }
 
-void number(const double& value,
+void set(
+  const char& idvaluesep,
+  const char* unit,
+  const uint8_t& unitlenght,
+  const uint8_t& fixedlength,
+  const uint8_t& width,
+  const uint8_t& precision) {
+  assignbuffer(unit_, unit, unitlenght_, unitlenght);
+  if (fixedlength < FORMAT_BUFFER_SIZE) {
+    fixedlenght_ = fixedlength;
+  }
+  idvaluesep_ = idvaluesep;
+  width_= width;
+  precision_ = precision;
+}
+
+void ids(const char* ids, const uint8_t& lenght) {
+  assignbuffer(ids_, ids, idslenght_, lenght);
+}
+
+void number(
+  const double& value,
   const uint8_t& index,
   const uint8_t& width,
   const uint8_t& precision,
   const uint8_t& start) {
-  if(FORMAT_BUFFER_SIZE - start - width > 0) {
-    dtostrf(value, width, precision, (formated[index]) + start);
+  if(unitlenght_ > 0 && unit_ != nullptr) {
+    memset(formated[index], 32, fixedlenght_);
+  }
+  size = FORMAT_BUFFER_SIZE - start - width - unitlenght_;
+  if(idslenght_ > 0) {
+    size -= 2;
+  }
+  if(size > 0) {
+    pointer = formated[index];
+    pointer += start;
+    if(idslenght_ > 0) {
+      pointer += 2;
+    }
+    widthtouse = width == FWIDTH ? width_ : width;
+    precisiontouse = precision == FPRECISION ? precision_ : precision;
+#ifdef WIN32
+    sprintf_s(pointer, FORMAT_BUFFER_SIZE - start, "%f", value);
+#else
+    dtostrf(value, widthtouse, precisiontouse, pointer);
+#endif
+    if(idslenght_ > 0) {
+      pointer = formated[index];
+      pointer[0] = ids_[index];
+      pointer[1] = idvaluesep_;
+    }
+    if(unitlenght_ > 0 && unit_ != nullptr) {
+      pointer = formated[index] + fixedlenght_ - unitlenght_;
+      memcpy(pointer, unit_, unitlenght_);
+    }
   }
 }
 
-char* get(const uint8_t& index){
+char* get(const uint8_t& index) {
   return formated[index];
+}
+
+void assign(const uint8_t& index, const char* text, const uint8_t& length) {
+  memcpy(formated[index], text, min(length, FORMAT_BUFFER_SIZE));
 }
 
 }
--- src/arduinoformat.h
+++ src/arduinoformat.h
@@ -1,13 +1,19 @@
 #ifndef _FDS_ARDUINO_LIBRARY_FORMAT_H_
 #define _FDS_ARDUINO_LIBRARY_FORMAT_H_
 
+#ifndef WIN32
 #include <Arduino.h>
+#endif
 
 #ifndef FORMAT_BUFFER_COUNT
 #define FORMAT_BUFFER_COUNT     2
 #endif
 #ifndef FORMAT_BUFFER_SIZE
 #define FORMAT_BUFFER_SIZE     32
+#endif
+
+#ifndef FORMAT_FIXED_LENGTH
+#define FORMAT_FIXED_LENGTH    12
 #endif
 
 #ifndef FORMAT_DOUBLE_WIDTH
@@ -17,16 +23,28 @@
 #define FORMAT_DOUBLE_PRECISION 2
 #endif
 
+#define FWIDTH 0
+#define FPRECISION 0
+
 namespace fds {
 namespace format {
 void setup();
+void set(
+  const char& idvaluesep,
+  const char* unit,
+  const uint8_t& unitlength,
+  const uint8_t& fixedlength = FORMAT_FIXED_LENGTH,
+  const uint8_t& width = FORMAT_DOUBLE_WIDTH,
+  const uint8_t& precision = FORMAT_DOUBLE_PRECISION);
+void ids(const char* ids, const uint8_t& lenght);
 void number(
   const double& value,
   const uint8_t& index = 0,
-  const uint8_t& width = FORMAT_DOUBLE_WIDTH,
-  const uint8_t& precision = FORMAT_DOUBLE_PRECISION,
+  const uint8_t& width = FWIDTH,
+  const uint8_t& precision = FPRECISION,
   const uint8_t& start = 0);
 char* get(const uint8_t& index = 0);
+void assign(const uint8_t& index, const char* text, const uint8_t& length);
 }
 }
 
